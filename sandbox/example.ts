0acb869b6a88b1e69dc948e16db7e4c9f7134d4b
michal <michal.a.kozakiewicz@gmail.com>
2026-01-03 21:57:35 +0100

    first commit

---
+++ sandbox/example.ts
@@ -0,0 +1,17 @@
+
+
+class Customer {
+
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+        name: string;
+        age: number;
+}
+let customer = new Customer("Alice", 30);
+console.log(customer.name);
+console.log("Hello, world2!");
+console.log("Hello, world!");
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,21 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>Welcome to {{title.toUpperCase()}}!</h1>
+    {{3+1}}
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'customer-manager';
+  title: string = 'customer-manager';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.title = 'customer-manager-updated after timeout';
+            }, 
+            3000);
+  }
+
 }
